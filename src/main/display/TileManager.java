c5b2af09713e9157d600a46a32d3acd5b90b021e
erakai <kaitinkess@gmail.com>
2021-09-18 14:06:40 -0400

    Rendering TileManager

--- src/main/display/TileManager.java
+++ src/main/display/TileManager.java
@@ -6,19 +6,43 @@
 
 package main.display;
 
-public class TileManager {
+import main.util.Globals;
+import main.util.Location;
+
+import java.awt.*;
+
+public class TileManager implements Renderable {
     private static Tile[][] tileArray;
     private static int rows, columns, fullSize;
 
     private TileManager() {
-        rows = 8;
-        columns = 8;
+        rows = (int) Globals.constant("ROW_#");
+        columns = (int) Globals.constant("COLUMN_#");
         fullSize = rows*columns;
         tileArray= new Tile[rows][columns];
+
+        populateTiles();
     }
 
-    public Tile getTile(int row, int column) {
-        return tileArray[row][column];
+    private void populateTiles() {
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                tileArray[i][j] = new Tile(new Location(i, j));
+            }
+        }
+    }
+
+    @Override
+    public void render(Graphics g) {
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                getTile(i, j).render(g);
+            }
+        }
+    }
+
+    public Tile getTile(int column, int row) {
+        return tileArray[column][row];
     }
 
     //accessors
