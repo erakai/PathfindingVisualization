97a77907354b0442a2bab3a0fa4ce7b958874f51
erakai <kaitinkess@gmail.com>
2021-09-18 15:46:57 -0400

    Initial BreadthFirst implementation

--- src/model/BreadthFirst.java
+++ src/model/BreadthFirst.java
@@ -1,8 +1,48 @@
 package model;
 
+import main.display.Tile;
+import model.service.Direction;
+import model.service.Node;
+
+import java.util.*;
+
 /**
  * Simplest pathfinding Algorithm
  * https://www.redblobgames.com/pathfinding/a-star/introduction.html
  */
 public class BreadthFirst {
+
+    public static List<Tile> runFloodFill(Node start, Node goal) {
+        Node current = start;
+        Queue<Node> frontier = new LinkedList<>();
+        Map<Node, Node> path = new HashMap<>();
+
+        frontier.add(current);
+        while (!frontier.isEmpty()) {
+            current = frontier.poll();
+            if (current == goal) break;
+            for (Node n: current.neighbors().keySet()) {
+                if (!path.containsValue(n)) {
+                    frontier.add(n);
+                    path.put(n, current);
+                }
+            }
+        }
+
+        current = goal;
+        List<Node> pathBack = new ArrayList<>();
+        while (current != start) {
+            pathBack.add(current);
+            current = path.get(current);
+        }
+        pathBack.add(start);
+
+        List<Tile> tiles = new ArrayList<>();
+        for (int i = pathBack.size()-1; i > 0; i--) {
+            tiles.add(pathBack.get(i).tile());
+        }
+
+        return tiles;
+    }
+
 }
--- src/model/service/Node.java
+++ src/model/service/Node.java
@@ -1,33 +1,45 @@
-package model;
+package model.service;
 
 import main.display.Tile;
 import main.display.TileManager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Node {
-    public List<Node> neighbors;
+    private Map<Node, Direction> neighbors;
+    private Tile tile;
 
     public Node(TileManager manager, Tile t) {
-        neighbors = new ArrayList<>();
+        this.tile = t;
+        neighbors = new HashMap<>();
 
         if (t.getLocation().getTileX() > 0) {
             Tile leftNeighbor = manager.getTile(t.getLocation().getTileX() - 1, t.getLocation().getTileY());
-            neighbors.add(new Node(manager, leftNeighbor));
+            if (!leftNeighbor.isOccupied()) neighbors.put(new Node(manager, leftNeighbor), Direction.LEFT);
         }
         if (t.getLocation().getTileX() < manager.getColumns()-1) {
             Tile rightNeighbor = manager.getTile(t.getLocation().getTileX()+1, t.getLocation().getTileY());
-            neighbors.add(new Node(manager, rightNeighbor));
+            if (!rightNeighbor.isOccupied()) neighbors.put(new Node(manager, rightNeighbor), Direction.RIGHT);
         }
         if (t.getLocation().getTileY() > 0) {
             Tile topNeighbor = manager.getTile(t.getLocation().getTileX(), t.getLocation().getTileY() - 1);
-            neighbors.add(new Node(manager, topNeighbor));
+            if (!topNeighbor.isOccupied()) neighbors.put(new Node(manager, topNeighbor), Direction.UP);
         }
         if (t.getLocation().getTileY() < manager.getRows()-1) {
             Tile bottomNeighbor = manager.getTile(t.getLocation().getTileX(), t.getLocation().getTileY() + 1);
-            neighbors.add(new Node(manager, bottomNeighbor));
+            if (!bottomNeighbor.isOccupied())  neighbors.put(new Node(manager, bottomNeighbor), Direction.DOWN);
         }
+    }
+
+    public Map<Node, Direction> neighbors() {
+        return neighbors;
+    }
+
+    public Tile tile() {
+        return tile;
     }
 
 }
