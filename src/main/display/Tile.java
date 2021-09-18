1e8d0e17ca37b4ee244eaadeac2fb0d16e0fd00d
Kian Nowrouzi <kian@pal-nat186-0-46.itap.purdue.edu>
2021-09-18 10:38:21 -0400

    Tile class creation

---
+++ src/main/display/Tile.java
@@ -0,0 +1,50 @@
+/*
+git add *
+git commit -m "commit name"
+git status
+*/
+
+package main.display;
+
+import java.awt.*;
+
+
+public class Tile {
+    private int x,y,width,height;
+
+    public void drawMe(Graphics g) {
+        g.fillRect(x,y,width,height);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
